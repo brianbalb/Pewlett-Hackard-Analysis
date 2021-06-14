631897b00aab02617fa5ae4e3d2595e3ec8ef746
brianbalb <brian.balbuena@outlook.com>
2021-06-13 22:03:11 -0400

    Update Challenge Files

---
+++ Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_Challenge.sql
@@ -0,0 +1,46 @@
+--- 1st Deliverable
+Select e.emp_no,
+    e.first_name,
+    e.last_name,
+    ti.title,
+    ti.from_date,
+    ti.to_date
+INTO retirement_titles
+FROM employees AS e
+LEFT JOIN titles AS ti
+ON e.emp_no = ti.emp_no
+WHERE (e.birth_date between '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+--- Use Distinct Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title,
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+
+--- Find number of employees by most recent job title that are about to retire
+SELECT COUNT(title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+--- 2nd Diliverable 
+--- Create table of employees eligible for mentorship program
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+    e.first_name,
+    e.last_name,
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    ti.title
+INTO mentorship_eligibility
+FROM employee AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1065-01=01' AND '1965-12-31')
+    AND (de.to_date = '9999-01-01')
+ORDER BY emp_no;
